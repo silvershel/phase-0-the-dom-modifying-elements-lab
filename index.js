a8e2c983d41edf116ce5fdb24dc324f138ffd7ee
Shelli Martinez <shelli.mrtnz@gmail.com>
2023-10-18 00:51:14 -0400

    add completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+const yourName = 'SHELLI';
+newHeader.textContent = `${yourName} is the champion`;
+document.body.appendChild(newHeader);
